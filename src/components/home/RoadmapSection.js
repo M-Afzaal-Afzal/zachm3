ec8fb72b6aa90d5c52675e76940cf2b4d89d1902
M Afzaal Afzal <afzaal1721@gmail.com>
2021-10-24 18:17:54 +0500

    done with the animations

--- src/components/home/RoadmapSection.js
+++ src/components/home/RoadmapSection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Box, Container, Grid} from "@mui/material";
 import StepWithDescription from "../common/StepWithDescription";
 import HeadingWithDescription from "../common/HeadingWithDescription";
+import Fade from 'react-reveal/Fade';
+
 
 const RoadmapSection = () => {
     return (
@@ -12,7 +14,7 @@ const RoadmapSection = () => {
         >
             <Container maxWidth={'xxl'}>
                 <Box>
-                    <Grid container >
+                    <Grid container>
 
                         <Grid item xs={12} xl={6}>
                             <Box>
@@ -30,46 +32,49 @@ const RoadmapSection = () => {
                         }}>
                             <Box sx={{display: 'grid', gridGap: '1.3rem', height: '100%'}}>
 
-                                <StepWithDescription
-                                    heading={'10%'}
-                                    description={'Giveaways and launch!!!'}
-                                />
-                                <StepWithDescription
-                                    heading={'20%'}
-                                    description={'Collab Fangsters by NFT artists - auctions and exclusive drops to holders'}
-                                />
-                                <StepWithDescription
-                                    heading={'30%'}
-                                    description={'1st charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'40%'}
-                                    description={'ETH and NFT giveaways to holders'}
-                                />
-                                <StepWithDescription
-                                    heading={'50%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'60%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'70%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'80%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'90%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
-                                <StepWithDescription
-                                    heading={'100%'}
-                                    description={'Premium Merch Store and 2nd charity donation chosen by community'}
-                                />
+                                <Fade delay={200} duration={3000} left>
+
+                                    <StepWithDescription
+                                        heading={'10%'}
+                                        description={'Giveaways and launch!!!'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'20%'}
+                                        description={'Collab Fangsters by NFT artists - auctions and exclusive drops to holders'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'30%'}
+                                        description={'1st charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'40%'}
+                                        description={'ETH and NFT giveaways to holders'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'50%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'60%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'70%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'80%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'90%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                    <StepWithDescription
+                                        heading={'100%'}
+                                        description={'Premium Merch Store and 2nd charity donation chosen by community'}
+                                    />
+                                </Fade>
                             </Box>
                         </Grid>
 
